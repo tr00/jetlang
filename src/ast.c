5b404b7c07e5bee737703771b9c523395787998f
tr00 <tryyyy@gmx.de>
2022-07-08 09:55:22 +0200

    pretty printing

--- src/ast.c
+++ src/ast.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -31,4 +32,28 @@ ast_node_t *ast_create_atom(enum AST_ATOM_HEAD head, const char *str, size_t len
     };
 
     return node;
+}
+
+void ast_pretty_print(ast_node_t *ptr)
+{
+    static const char *atom2str[] = {
+        [AST_ATOM_HEAD_NIL] = "nil",
+        [AST_ATOM_HEAD_SYM] = "sym",
+        [AST_ATOM_HEAD_INT] = "int",
+    };
+
+    ast_node_t *node;
+    int flag;
+
+    flag = ast_node_typeof(ptr);
+    node = ast_node_decode(ptr);
+
+    if (flag == AST_NODE_FLAG_ATOM)
+    {
+        ast_atom_t *atom = &node->atom;
+        printf("<atom:%s> %s", atom2str[atom->head], atom->body.str);
+    }
+    else
+    {
+    }
 }
