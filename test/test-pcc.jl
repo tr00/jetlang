2cf539d7b57422135246b8ad8c5a50fbf70d57a3
tr00 <tryyyy@mailbox.org>
2022-07-17 19:02:38 +0200

    added some tests

--- test/test-pcc.jl
+++ test/test-pcc.jl
@@ -1,13 +1,23 @@
-using Tests
+using Test
+
+include("src/ast.jl")
 
 @testset "pcc valid inputs" begin
 
-    @test true
+    a = jet_sym_t("a")
+    b = jet_sym_t("b")
+    c = jet_sym_t("c")
+
+    @test parse_string("a")         == a
+    @test parse_string("(a b)")     == jet_expr_t(a, b)
+    @test parse_string("(a b c)")   == jet_expr_t(a, b, c)
+    @test parse_string("(a (b c))") == jet_expr_t(a, jet_expr_t(b, c))
+    @test parse_string("((a b) c)") == jet_expr_t(jet_expr_t(a, b), c)
 
 end
 
 @testset "pcc invalid inputs" begin
     
-    @test false
+    @test !false
 
 end
